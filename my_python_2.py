e27118c9c00266f6d6fbca6f676484be0a5dd342
Evgeny Golichenkov <neonmaster22@yandex.ru>
2023-12-23 15:59:46 +0300

    [#045] commit

--- my_python_2.py
+++ my_python_2.py
@@ -50,6 +50,86 @@ print(found_coins + magic_coins * 365 - stolen_coins * 52)
 magic_coins = 13
 print(found_coins + magic_coins * 365 - stolen_coins * 52)
 """
+"""
+fred = "Почему у горил большие ноздри? Потому что у горил толстые пальцы!"
+print(fred)
+fred = "Что это розовое пушистое? Розовый пушистик!"
+print(fred)
+fred = '''Это едят на полдник динозавры?
+ТиРекс-кекс!'''
+print(fred)
+"""
+"""
+silly_string = '"Здесь что-то не так, не будь я д\'Артаньян" - подумал он.'
+print(silly_string)
+silly_string = "\"Здесь что-то не так, не будь я д'Артаньян\" - подумал он."
+print(silly_string)
+silly_string = '''"Здесь что-то не так, не будь я д'Артаньян" - подумал он.'''
+print(silly_string)
+"""
+"""
+myscore = 1000
+message = 'Мой счет: %s очков'
+print(message % myscore)
+"""
+"""
+joke_text = "%s: приспособление для поиста мебели в темноте"
+bodypart1 = 'Коленка'
+bodypart2 = 'Лодышка'
+print(joke_text % bodypart1)
+print(joke_text % bodypart2)
+"""
+"""
+nums = "Что сказало число %s числу %s? Славный поясок!"
+print(nums % (0, 8))
+"""
+"""
+print(10 * "a")
+"""
+"""
+spaces = " " * 25
+print("%s Задний переулок 12" % spaces)
+print("%s Трясогузочья пустошь" % spaces)
+print("%s Западный скрапшир" % spaces)
+print()
+print()
+print("уважаемый Сэр,")
+print()
+print('Хочу сообщитьвам, что кое-где на крыше уборной')
+print("недостает кусочков черепицы.")
+print("Думаю прошлой ночью их сдуло внезапным порывом ветра.")
+print()
+print("С почтением")
+print("Мальком Конфузли")
+"""
+"""
+print(1000 * "слякоть")
+"""
+"""
+wizard_list = "паучьи лапки, жабий палец, глаз тритона, крыло летучий мыши, жир слизня, перхоть змей"
+print(wizard_list)
+
+wizard_list = ["паучьи лапки", "жабий палец", "глаз тритона", "крыло летучий мыши", "жир слизня", "перхоть змей"]
+print(wizard_list)
+print(wizard_list[2])
+
+wizard_list[2] = "язык улитки"
+print(wizard_list)
+
+print(wizard_list[2:5])
+"""
+"""
+some_numbers = [1, 2, 5, 10, 20]
+print(some_numbers)
+some_strings = ["нож", "отточен", "тожен", "очень"]
+print(some_strings)
+numbers_and_strings = [7, "раз", "отпей", 1, "раз", "отешь"]
+print(numbers_and_strings)
+"""
+
+
+
+
 
 
 
